ee9a8b85b6f48787818d0ede351abb0549ec73c8
benjamin1521 <veniamin1601@gmail.com>
2019-12-02 13:57:24 +0200

    Added starter class, Feature completed

--- src/main/java/ua/training/Game.java
+++ src/main/java/ua/training/Game.java
@@ -1,8 +1,12 @@
 package ua.training;
 
+import java.util.Scanner;
+
 public class Game {
     private static final int WIN_CELL = 100;
+    private static final int START_CELL = 1;
     private Player player;
+    private Dice dice = new Dice();
 
     public Game(Player player) {
         this.player = player;
@@ -20,7 +24,30 @@ public class Game {
         return WIN_CELL == position;
     }
 
-    public static void main(String[] args) {
+    public void startGame() {
+        Scanner s = new Scanner(System.in);
+        String str;
+        int diceValue;
 
+        do {
+            System.out.println("Press 1 to roll Dice");
+            str = s.next();
+
+            diceValue = dice.roll();
+            System.out.println("You rolled " + diceValue);
+
+            movePlayer(player, diceValue);
+            System.out.println("Position: " + player.getPosition() + "\n");
+
+            if (isWin(player.getPosition())) {
+                System.out.println("Player " + player.getId() + " wins!");
+                return;
+            }
+
+        } while ("1".equals(str));
+    }
+
+    public static void main(String[] args) {
+        new Game(new Player(0, START_CELL)).startGame();
     }
 }
